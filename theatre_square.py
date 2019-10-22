48261d3f6fa1ab433dc149c14d16552be9a3a472
Anand Patel <52890488+devtechnocode@users.noreply.github.com>
2019-10-23 00:44:10 +0530

    Update theatre_square.py

--- theatre_square.py
+++ theatre_square.py
@@ -1,1 +1,26 @@
-
+n,m,a=[int(b) for b in input().split()]
+if n%a==0 and m%a==0:
+    c=n//a
+    d=m//a
+    e=c*d
+    print (e)
+ 
+ 
+elif n%a==0 and m%a!=0:
+    c=n//a
+    d=m//a+1
+    e=c*d
+    print (e)
+ 
+elif n%a!=0 and m%a==0:
+    c=n//a+1
+    d=m//a
+    e=c*d
+    print (e)
+ 
+ 
+elif n%a!=0 and m%a!=0:
+    c=n//a+1
+    d=m//a+1
+    e=c*d
+    print (e)
